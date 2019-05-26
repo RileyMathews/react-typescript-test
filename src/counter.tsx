2972ec3bc217ec2c331bf94583c795d7757a7a9a
Riley Mathews <rileymathews80@gmail.com>
2019-05-26 14:28:38 -0500

    added basic example of using typed state and props

---
+++ src/counter.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+interface IProps {}
+
+interface IState {
+    count: number;
+}
+
+export default class Counter extends React.Component<IProps, IState> {
+  state: IState = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: (this.state.count + 1)
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: (this.state.count - 1)
+    });
+  };
+
+  render () {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
